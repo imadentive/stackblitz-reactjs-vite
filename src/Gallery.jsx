dcec00ca8fd01a9346a879848c66393c752d62df
luoweilong <luoweilong@fadada.com>
2024-03-26 11:07:14 +0800

    init

---
+++ src/Gallery.jsx
@@ -0,0 +1,16 @@
+function Profile() {
+    return (
+      <div>hello</div>
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>了不起的科学家们</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
