d6b7ff28c19d281bb52226c2396f083c179d810b
luoweilong <luoweilong@fadada.com>
2024-03-26 11:40:31 +0800

    yarn add antd-mobile

--- src/view/app/index.jsx
+++ src/view/app/index.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+// import { Button } from 'antd';
+import { Button } from "antd-mobile";
+import { Tag, Space } from 'antd-mobile'
 function Profile() {
   return <div>Profile</div>;
 }
@@ -12,6 +15,21 @@ function Gallery() {
         <Profile />
       </section>
       <Link to="about">About Us</Link>
+      <br />
+      <Button size="mini" color="primary">
+        Mini
+      </Button>
+      <Space>
+          <Tag color='primary' fill='outline'>
+            Primary
+          </Tag>
+          <Tag color='#87d068' fill='outline'>
+            #87d068
+          </Tag>
+          <Tag color='#ff6430' fill='outline'>
+            #ff6430
+          </Tag>
+        </Space>
     </>
   );
 }
